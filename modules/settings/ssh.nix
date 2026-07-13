456d58193fd48a647d3b310d25ca4a5754d8e528
Luna <quocjq@gmail.com>
2026-07-13 22:19:05 +0700

    change stuff

--- modules/settings/ssh.nix
+++ modules/settings/ssh.nix
@@ -2,14 +2,16 @@
   den.aspects.ssh = {
     nixos = {
       services.openssh.enable = true;
-      programs.ssh.startAgent = true;
-      extraConfig = ''
-        Host oraclevps
-          HostName 217.142.232.103
-          User ubuntu
-          IdentityFile ~/.ssh/ssh-key-2026-05-09.key
-        AddKeysToAgent yes
-      '';
+      programs.ssh = {
+        startAgent = true;
+        extraConfig = ''
+          Host oraclevps
+            HostName 217.142.232.103
+            User ubuntu
+            IdentityFile ~/.ssh/ssh-key-2026-05-09.key
+          AddKeysToAgent yes
+        '';
+      };
     };
   };
 }
