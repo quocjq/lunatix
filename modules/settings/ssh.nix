b6518c24a40f120310447b16f6bf580dc2da0a08
Luna <quocjq@gmail.com>
2026-07-13 22:15:57 +0700

    add config nix

--- modules/settings/ssh.nix
+++ modules/settings/ssh.nix
@@ -3,6 +3,13 @@
     nixos = {
       services.openssh.enable = true;
       programs.ssh.startAgent = true;
+      extraConfig = ''
+        Host oraclevps
+          HostName 217.142.232.103
+          User ubuntu
+          IdentityFile ~/.ssh/ssh-key-2026-05-09.key
+        AddKeysToAgent yes
+      '';
     };
   };
 }
