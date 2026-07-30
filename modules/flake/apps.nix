8a33e8ab61b4eeaf71dc1bb78c7af3ebc2c522a0
Luna <quocjq@gmail.com>
2026-07-31 00:02:07 +0700

    use nixos facter instead of default nixos-generated

--- modules/flake/apps.nix
+++ modules/flake/apps.nix
@@ -7,10 +7,14 @@
     packages =
       let
         hm = inputs.self.nixosConfigurations.igloo.config.home-manager.users.lunixose;
+        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
       in
       {
         doomacs = hm.programs.doom-emacs.finalEmacsPackage;
         nixcord = hm.programs.nixcord.finalPackage.discord;
+        # `nix run .#facter -- -o modules/hardware/latitude3250.json` regenerates
+        # the hardware report consumed by modules/hardware/latitude3250.nix.
+        facter = pkgs.nixos-facter;
       };
   };
 }
--- modules/hardware/latitude3250.nix
+++ modules/hardware/latitude3250.nix
@@ -1,26 +1,12 @@
+{ inputs, ... }:
 {
   den.aspects.latitude3250 = {
-    nixos =
-      {
-        config,
-        pkgs,
-        lib,
-        modulesPath,
-        ...
-      }:
-      {
-        hardware.enableRedistributableFirmware = lib.mkDefault true;
-        boot.initrd.availableKernelModules = [
-          "xhci_pci"
-          "thunderbolt"
-          "ahci"
-          "nvme"
-          "ums_realtek"
-          "usb_storage"
-          "sd_mod"
-        ];
-        boot.initrd.kernelModules = [ ];
-        boot.kernelModules = [ "kvm-intel" ];
-      };
+    nixos = {
+      # imports = [
+      #   inputs.nixos-facter-modules.nixosModules.facter
+      # ];
+
+      hardware.facter.reportPath = ./latitude3250.json;
+    };
   };
 }
