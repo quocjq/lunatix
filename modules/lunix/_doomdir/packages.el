8bf42c7dbf16bcd3aa3b31d20caff64c9ad1973a
Luna <quocjq@gmail.com>
2026-07-30 22:14:39 +0700

    Doom config update

---
+++ modules/lunix/_doomdir/packages.el
@@ -0,0 +1,30 @@
+(package! org-modern)
+
+(package! org-appear)
+
+(package! org-fragtog)
+
+(package! polymode)
+(package! poly-org)
+
+(package! org-super-agenda)
+
+(package! denote)
+(package! denote-org)
+(package! denote-sequence)
+(package! denote-journal)
+(package! denote-merge
+  :recipe (:host github :repo "protesilaos/denote-merge")
+  :pin "2f8d168d37e66b5c8a3b983e211031b9abbf402e")
+(package! denote-silo)
+(package! denote-menu)
+(package! consult-denote)
+
+(package! theme-magic)
+(package! svg-tag-mode)
+(package! doct)
+(package! org-noter)
+
+(package! olivetti)
+
+(package! show-font)
