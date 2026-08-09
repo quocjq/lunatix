7cf4a515badd8a09e26c86c5ab07e3d3bf618640
Luna <quocjq@gmail.com>
2026-08-09 17:00:43 +0700

    fix(server): website service user + data dir via tmpfiles

--- modules/community/lix/services/website.nix
+++ modules/community/lix/services/website.nix
@@ -14,6 +14,16 @@
         pkg = inputs.website.packages.${pkgs.stdenv.hostPlatform.system}.default;
       in
       {
+        users.users.www-data = {
+          isSystemUser = true;
+          group = "www-data";
+        };
+        users.groups.www-data = { };
+        systemd.tmpfiles.rules = [
+          "d /srv/www 0755 root root - -"
+          "d /srv/www/data 0755 www-data www-data - -"
+        ];
+
         systemd.services.lunatix-website = {
           description = "lunatix Nuxt dashboard";
           after = [ "network.target" ];
@@ -26,12 +36,13 @@
           };
           serviceConfig = {
             Type = "exec";
+            ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /srv/www/data";
             ExecStart = "${pkgs.nodejs}/bin/node ${pkg}/share/lunatix-website/.output/server/index.mjs";
             WorkingDirectory = "${pkg}/share/lunatix-website/.output/server";
             Restart = "on-failure";
             RestartSec = 3;
-            DynamicUser = true;
-            StateDirectory = [ "www/data" ];
+            User = "www-data";
+            Group = "www-data";
           };
         };
       };
